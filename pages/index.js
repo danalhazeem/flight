7d9435079597d1349d85f2f948de47e4c32c713e
Wrik Das <wrikdas22@gmail.com>
2023-02-22 19:24:48 +0530

    renamed event handlers and updated flight component with string action

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
 import { useState } from 'react'
 import Head from 'next/head'
+import Flights from '@/components/flights'
 import styles from '@/styles/Home.module.css'
 import flightService from '@/services/flight.service'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Flights from '@/components/flights'
 
-export default function Home() {
+export default function Flight() {
   const [from, setFrom] = useState('')
   const [to, setTo] = useState('')
   const [date, setDate] = useState('')
   const [flights, setFlights] = useState([])
 
-  function flightSearchHandler(event) {
+  function handleSubmit(event) {
     event.preventDefault()
     flightService.getFlights(from, to, date)
       .then((flights) => setFlights(flights))
       .catch((error) => console.error(error))
   }
 
-  const changeFrom = (event) => setFrom(event.target.value)
-  const changeTo = (event) => setTo(event.target.value)
-  const changeDate = (event) => setDate(event.target.value)
+  const handleFromChange = (event) => setFrom(event.target.value)
+  const handleToChange = (event) => setTo(event.target.value)
+  const handleDateChange = (event) => setDate(event.target.value)
 
   return (
     <>
@@ -32,13 +32,13 @@ export default function Home() {
       <main className='container d-flex flex-column align-items-center mt-4 pt-4'>
         <h1 className={`${styles.heading} boldest col`}>Flights</h1>
         <div className='col mt-4'>
-          <form className='row-6 input-group mt-4' onSubmit={flightSearchHandler}>
-            <input type='text' aria-label='From Location' className='form-control' placeholder='From' value={from} onChange={changeFrom} required />
-            <input type='text' aria-label='To Location' className='form-control' placeholder='To' value={to} onChange={changeTo} required />
-            <input type='date' aria-label='Date Of Travel' className='form-control' placeholder='Date' value={date} onChange={changeDate} required />
+          <form className='row-6 input-group mt-4' onSubmit={handleSubmit}>
+            <input type='text' aria-label='From Location' className='form-control' placeholder='From' value={from} onChange={handleFromChange} required />
+            <input type='text' aria-label='To Location' className='form-control' placeholder='To' value={to} onChange={handleToChange} required />
+            <input type='date' aria-label='Date Of Travel' className='form-control' placeholder='Date' value={date} onChange={handleDateChange} required />
             <input type='submit' className='btn btn-dark' value='Search' />
           </form>
-          {flights.length == 0 ? null : <Flights flights={flights} actionIsBook={true} />}
+          {flights.length == 0 ? null : <Flights flights={flights} action="book" />}
         </div>
       </main>
     </>

915ade03206c2dcfc8d02289e5a46255238f4b89
Wrik Das <wrikdas22@gmail.com>
2023-02-22 19:25:31 +0530

    added service method for fetching a single flight

--- services/flight.service.js
+++ services/flight.service.js
@@ -2,10 +2,15 @@ import axios from 'axios'
 
 const base = 'http://localhost:8080/flight'
 
+const getFlight = (id) => {
+    const promise = axios.get(`${base}/${id}`)
+    return promise.then((response) => response.data)
+}
+
 const getFlights = (from, to, date) => {
     const promise = axios.get(`${base}/${from}/${to}/${date}`)
     return promise.then((response) => response.data)
 }
 
-const flightService = { getFlights }
+const flightService = { getFlight, getFlights }
 export default flightService

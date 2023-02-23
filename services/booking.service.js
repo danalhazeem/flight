5cd61867da82a5ae9f2e47b50da9c882bca82691
Wrik Das <wrikdas22@gmail.com>
2023-02-23 14:48:27 +0530

    replaced hardcoded tokens used in testing with actual auth token implementation

--- services/booking.service.js
+++ services/booking.service.js
@@ -3,29 +3,20 @@ import axios from 'axios'
 const base = 'http://localhost:8080/booking'
 
 const getBookings = (token) => {
-    const headers = {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFkbWluMTIzQGdtYWlsLmNvbSIsInVzZXJJZCI6MSwiaWF0IjoxNjc3MTMwOTkxLCJleHAiOjE2NzcxMzQ1OTF9.KXNZjfsVadMr8iMn_ZbiACErmqIZ8tPzbapoImnyPHQ'
-    }
-
+    const headers = { Authorization: `Bearer ${token}` }
     const promise = axios.get(base, { headers })
     return promise.then((response) => response.data)
 }
 
 const createBooking = (flightId, firstName, lastName, token) => {
-    const headers = {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFkbWluMTIzQGdtYWlsLmNvbSIsInVzZXJJZCI6MSwiaWF0IjoxNjc3MTMwOTkxLCJleHAiOjE2NzcxMzQ1OTF9.KXNZjfsVadMr8iMn_ZbiACErmqIZ8tPzbapoImnyPHQ'
-    }
+    const headers = { Authorization: `Bearer ${token}` }
     const body = { flightId, firstName, lastName }
-
     const promise = axios.post(base, body, { headers })
     return promise.then((response) => response.data)
 }
 
 const deleteBooking = (id, token) => {
-    const headers = {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFkbWluMTIzQGdtYWlsLmNvbSIsInVzZXJJZCI6MSwiaWF0IjoxNjc3MTMwOTkxLCJleHAiOjE2NzcxMzQ1OTF9.KXNZjfsVadMr8iMn_ZbiACErmqIZ8tPzbapoImnyPHQ'
-    }
-
+    const headers = { Authorization: `Bearer ${token}` }
     const promise = axios.delete(`${base}/${id}`, { headers })
     return promise.then((response) => response.data)
 }
--- services/checkin.service.js
+++ services/checkin.service.js
@@ -3,11 +3,8 @@ import axios from 'axios'
 const base = 'http://localhost:8080/checkin'
 
 const checkin = (bookingId, token) => {
-    const headers = {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFkbWluMTIzQGdtYWlsLmNvbSIsInVzZXJJZCI6MSwiaWF0IjoxNjc3MTMwOTkxLCJleHAiOjE2NzcxMzQ1OTF9.KXNZjfsVadMr8iMn_ZbiACErmqIZ8tPzbapoImnyPHQ'
-    }
+    const headers = { Authorization: `Bearer ${token}` }
     const body = { bookingId }
-
     const promise = axios.post(base, body, { headers })
     return promise.then((response) => response.data)
 }

50ffee0e4db721b639f9095cf4446b25d3517030
Wrik Das <wrikdas22@gmail.com>
2023-02-23 14:45:27 +0530

    implemented user state, loading auth info from localstorage, providing user context

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import UserContext from '@/contexts/user'
 import '@/styles/globals.css'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [user, setUser] = useState({
+        token: null,
+    })
+
+    useEffect(() => {
+        const loggedInUserJSON = window.localStorage.getItem('fbs-user')
+        if (loggedInUserJSON) {
+            const user = JSON.parse(loggedInUserJSON)
+            setUser(user)
+        }
+    }, [])
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            <Component {...pageProps} />
+        </UserContext.Provider>
+    )
 }

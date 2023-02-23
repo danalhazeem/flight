44734e61d5cb1dace9a61e39c6baa89ac1531062
Wrik Das <wrikdas22@gmail.com>
2023-02-23 12:15:56 +0530

    updated booking service api

--- services/booking.service.js
+++ services/booking.service.js
@@ -2,19 +2,32 @@ import axios from 'axios'
 
 const base = 'http://localhost:8080/booking'
 
-const getBookings = () => {
-}
+const getBookings = (token) => {
+    const headers = {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFkbWluMTIzQGdtYWlsLmNvbSIsInVzZXJJZCI6MSwiaWF0IjoxNjc3MTMwOTkxLCJleHAiOjE2NzcxMzQ1OTF9.KXNZjfsVadMr8iMn_ZbiACErmqIZ8tPzbapoImnyPHQ'
+    }
 
-const createBooking = (body) => {
-    const promise = axios.post(base, body, {
-        headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW4xMjNAZ21haWwuY29tIiwiaWF0IjoxNjc3MDczNTE0LCJleHAiOjE2NzcwNzcxMTR9.nnHxybG2quDe7XgiOhlM7AzvMsyNlNyY4h_-1htqAjY'
-        }
-    })
+    const promise = axios.get(base, { headers })
     return promise.then((response) => response.data)
 }
 
-const deleteBooking = () => {
+const createBooking = (flightId, firstName, lastName, token) => {
+    const headers = {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFkbWluMTIzQGdtYWlsLmNvbSIsInVzZXJJZCI6MSwiaWF0IjoxNjc3MTMwOTkxLCJleHAiOjE2NzcxMzQ1OTF9.KXNZjfsVadMr8iMn_ZbiACErmqIZ8tPzbapoImnyPHQ'
+    }
+    const body = { flightId, firstName, lastName }
+
+    const promise = axios.post(base, body, { headers })
+    return promise.then((response) => response.data)
+}
+
+const deleteBooking = (id, token) => {
+    const headers = {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFkbWluMTIzQGdtYWlsLmNvbSIsInVzZXJJZCI6MSwiaWF0IjoxNjc3MTMwOTkxLCJleHAiOjE2NzcxMzQ1OTF9.KXNZjfsVadMr8iMn_ZbiACErmqIZ8tPzbapoImnyPHQ'
+    }
+
+    const promise = axios.delete(`${base}/${id}`, { headers })
+    return promise.then((response) => response.data)
 }
 
 const bookingService = { getBookings, createBooking, deleteBooking }
